58dbc9cefd524d10ed3619c0375bd177d9303cfc
gil mauro <gil_mauro@hotmail.com>
2022-10-12 20:04:34 -0300

    Agrege un Objecto pasajeros y modifique el codigo para que el usurio ingrese los datos de los pasajeros y en cuantas cuotas quiere abonar

--- precios.js
+++ precios.js
@@ -1,61 +1,75 @@
-function precioMenor(precio,edadMenor){
-    if (edadMenor<=2){
-        //menor abona el 10%
+function precioMenor(precio,edad){
+    if (edad<=2){
+        //menor de 2 abona el 10%
         return precio*0.10;
-    }else if (edadMenor<=12){
-        //menor abona el 50%
+    }else if (edad<=12){
+        //menor de 12 abona el 50%
         return precio/2;
     }else{
-        //menor abona como adulto
         return precio;
     }
 }
-let precio =parseFloat(prompt("Ingrese el precio del paquete"));
-let precioTotal=0;
-let adultos =parseInt(prompt("Ingrese la cantidad de adultos"));
-let menores =parseInt(prompt("Ingrese la cantidad de menores (Ingrese '(0) cero' si no viajan menores)"));
-let edadMenor;
-if(menores){
-    for (let i=1;i<=menores;i++){
-        edadMenor =parseInt(prompt("Ingrese la edad del menor "+i));
-        precioTotal+=precioMenor(precio,edadMenor);
+class pasajeros {
+    constructor (nombre, edad){
+        this.nombre =nombre;
+        this.edad =edad;
     }
 }
-precioTotal +=precio*adultos;
-console.log("El precio total es de $"+precioTotal+" para "+adultos+" adultos y "+menores+" menores.\n\n");
 
-for (let i=1;i<=12;i++){
-    let precioCuota=0;
-    switch (i){
-        case 1:
-            console.log("Precio en "+i+" pago es de $"+precioTotal+"\n\n");
-            break;
-        case 2:
-        case 3:
-            precioCuota = precioTotal + (precioTotal*0,03);
-            console.log("Precio en "+i+" pagos es de $"+precioCuota);
-            console.log("Cuotas de $"+(precioCuota/i).toFixed(2)+". Interes (3%)\n\n");
-            break;
-        case 4:
-        case 5:
-        case 6:
-            precioCuota = precioTotal + (precioTotal*0,05);
-            console.log("Precio en "+i+" pagos es de $"+precioCuota);
-            console.log("Cuotas de $"+(precioCuota/i).toFixed(2)+". Interes (5%)\n\n");
-            break;
-        case 7:
-        case 8:
-        case 9:
-            precioCuota = precioTotal + (precioTotal*0,10);
-            console.log("Precio en "+i+" pagos es de $"+precioCuota);
-            console.log("Cuotas de $"+(precioCuota/i).toFixed(2)+". Interes (10%)\n\n");
-            break;
-        case 10:
-        case 11:
-        case 12:
-            precioCuota = precioTotal + (precioTotal*0,15);
-            console.log("Precio en "+i+" pagos es de $"+precioCuota);
-            console.log("Cuotas de $"+(precioCuota/i).toFixed(2)+". Interes (15%)\n\n");
-            break;
+let precio =parseFloat(prompt("Ingrese el precio del paquete"));
+let precioTotal=0;
+let precioCuota=0;
+let cantPasajeros =parseInt(prompt("Ingrese la cantidad de pasajeros"));
+let listaPasajeros = [];
+for (let i=1;i<=cantPasajeros;i++){
+    let nombre = prompt("Ingrese el nombre del pasajero "+i);
+    let edad = parseInt(prompt("Ingrese la edad del pasajero "+i));
+    const pasajero = new pasajeros (nombre,edad);
+    listaPasajeros.push(pasajero)
+    precioTotal += precioMenor(precio,edad);
+}
+let cantCuotas =parseInt(prompt("En cuantas cuotas quiere abonar? (Entre 1-12 cuotas)"));
+
+for (const pasajero in listaPasajeros ){
+    if (listaPasajeros[pasajero].edad<=2){
+        console.log("El pasajero "+listaPasajeros[pasajero].nombre+" es menor de 2 abona el 10% $"+precio*0.10);
+    }else if (listaPasajeros[pasajero].edad<=12){
+        console.log("El pasajero "+listaPasajeros[pasajero].nombre+" es menor de 12 abona el 50% $"+precio/2);
+    }else{
+        console.log("El pasajero "+listaPasajeros[pasajero].nombre+" abona $"+precio);
     }
+}
+console.log("El precio total es de $"+precioTotal+"\n\n");
+
+switch (cantCuotas){
+    case 1:
+        console.log("Precio en "+cantCuotas+" pago $"+precioTotal+"\n\n");
+        break;
+    case 2:
+    case 3:
+        precioCuota = precioTotal + (precioTotal*0,03);
+        console.log("Precio en "+cantCuotas+" pagos $"+precioCuota);
+        console.log("Cuotas de $"+(precioCuota/cantCuotas).toFixed(2)+". Interes (3%)\n\n");
+        break;
+    case 4:
+    case 5:
+    case 6:
+        precioCuota = precioTotal + (precioTotal*0,05);
+        console.log("Precio en "+cantCuotas+" pagos $"+precioCuota);
+        console.log("Cuotas de $"+(precioCuota/cantCuotas).toFixed(2)+". Interes (5%)\n\n");
+        break;
+    case 7:
+    case 8:
+    case 9:
+        precioCuota = precioTotal + (precioTotal*0,10);
+        console.log("Precio en "+cantCuotas+" pagos $"+precioCuota);
+        console.log("Cuotas de $"+(precioCuota/cantCuotas).toFixed(2)+". Interes (10%)\n\n");
+        break;
+    case 10:
+    case 11:
+    case 12:
+        precioCuota = precioTotal + (precioTotal*0,15);
+        console.log("Precio en "+cantCuotas+" pagos $"+precioCuota);
+        console.log("Cuotas de $"+(precioCuota/cantCuotas).toFixed(2)+". Interes (15%)\n\n");
+        break;
 }
